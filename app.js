3287ac1e006e33a2001882268ce9ac081a2f6068
Bojan Vreco <milosgak2020@gmail.com>
2023-03-12 14:02:06 -0500

    First commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const sequelize = require('./util/database');
 
@@ -15,6 +17,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const customerRoutes = require('./routes/customer');
 
+app.use(compression());
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'signatures')));
@@ -28,12 +32,13 @@ app.use(session({
 app.use('/admin', adminRoutes);
 app.use(customerRoutes);
 
+console.log(process.env.NODE_ENV);
 sequelize
   //.sync({ force: true })
   .sync()
   .then(result => {
     //console.log(result);
-    app.listen(3000);
+    app.listen( process.env.PORT ||3000);
   })
   .catch(err => {
     console.log(err);
--- util/database.js
+++ util/database.js
@@ -1,8 +1,19 @@
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'poladva1', {
   dialect: 'mysql',
   host: 'localhost'
 });
 
+module.exports = sequelize; */
+
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(`${process.env.MYSQL_DB}`, `${process.env.MYSQL_USER}`, `${process.env.MYSQL_PWD}`, {
+  dialect: 'mysql',
+  host: `${process.env.MYSQL_LINK}`
+});
+
 module.exports = sequelize;
+
