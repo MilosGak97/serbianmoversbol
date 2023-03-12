97b883196b1228090bf3baf2df9e5df9d352fe96
Bojan Vreco <milosgak2020@gmail.com>
2023-03-12 16:02:57 -0500

    added express-session

--- util/database.js
+++ util/database.js
@@ -12,7 +12,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(`${process.env.MYSQL_DB}`, `${process.env.MYSQL_USER}`, `${process.env.MYSQL_PWD}`, {
   dialect: 'mysql',
-  host: `${process.env.MYSQL_LINK}`
+  host: 'localhost'
 });
 
 module.exports = sequelize;
