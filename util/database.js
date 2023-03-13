a8730565a6772eed03c9c09993f5c66b8b71de29
Bojan Vreco <milosgak2020@gmail.com>
2023-03-13 08:22:43 -0500

    added express-session

--- util/database.js
+++ util/database.js
@@ -1,23 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('d73o9ogsm261ep', 'mvablweobsgwzb', '23acf462ca2ee6ad45035ce4e9c8152b50c5e39a4fed5d9c5039a4c9d9192561', {
-  dialect: 'mysql',
-  host: 'ec2-34-226-11-94.compute-1.amazonaws.com',
-  port: 5432
-});
-
-module.exports = sequelize; 
-
-/*
-const Sequelize = require('sequelize');
-
 const sequelize = new Sequelize(`${process.env.MYSQL_DB}`, `${process.env.MYSQL_USER}`, `${process.env.MYSQL_PWD}`, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: `${process.env.MYSQL_HOST}`
 });
 
 module.exports = sequelize;
-*/
+
 
 
 /*const Sequelize = require('sequelize');
