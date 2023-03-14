b90ba7a41cc7cf764a79ed742dcf01903ca1fc18
Bojan Vreco <milosgak2020@gmail.com>
2023-03-13 19:46:35 -0500

    Test commit

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'milosgak', 'TheDVTN2020', 'mysql', {
-  host: 'mydb.cpi6e39gnpl9.us-east-2.rds.amazonaws.com',
+const sequelize = new Sequelize('node-complete', 'admin', 'TheDVTN2020', 'mysql', {
+  host: 'serbianmoversllc.cpi6e39gnpl9.us-east-2.rds.amazonaws.com',
   port: 3306
 });
 
