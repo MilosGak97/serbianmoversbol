f6d01f7efab4b22e8372b0fd0467708165617bb5
Bojan Vreco <milosgak2020@gmail.com>
2023-03-14 03:02:38 -0500

    New day commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session); /* test */
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const compression = require('compression');
@@ -24,6 +25,33 @@ const accessLogStream = fs.createWriteStream(
     flags: 'a'
   }
 )
+/* test */
+const options = {
+  host: 'mydb.cpi6e39gnpl9.us-east-2.rds.amazonaws.com',
+  user: 'milosgak',
+  password: 'TheDVTN2020',
+  database: 'node-complete',
+  port: 3306,
+  schema: {
+      tableName: 'sessions',
+      columnNames: {
+          session_id: 'session_id',
+          expires: 'expires',
+          data: 'data'
+      }
+  }
+}; /* test end */
+
+const store = new MySQLStore(options); /* test */
+
+app.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
+ /* test end with code above */
+
 
 app.use(compression());
 app.use(helmet());
@@ -34,12 +62,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'signatures')));
 
 
-
+/*
 app.use(session({
   secret: 'your_secret_key_here',
   resave: false,
   saveUninitialized: false
 }));
+*/
 
 app.use('/admin', adminRoutes);
 app.use(customerRoutes);
--- util/database.js
+++ util/database.js
@@ -1,3 +1,5 @@
+/* RADI */
+/*
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'admin', 'TheDVTN2020',  {
@@ -6,7 +8,18 @@ const sequelize = new Sequelize('node-complete', 'admin', 'TheDVTN2020',  {
   port: 3306
 });
 
+module.exports = sequelize; */
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'milosgak', 'TheDVTN2020',  {
+  dialect: 'mysql',
+  host: 'mydb.cpi6e39gnpl9.us-east-2.rds.amazonaws.com',
+  port: 3306
+});
+
 module.exports = sequelize;
+
 
 
 
